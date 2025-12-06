8937534903a650d499f88cfc612f46823dda4c5c
Chirag Jain <cjain9975@gmail.com>
2025-12-06 10:44:32 +0530

    Python Websocket Tutorial

---
+++ lesson1_text_binary/server_text.py
@@ -0,0 +1,36 @@
+import asyncio
+import websockets
+from websockets.exceptions import ConnectionClosedOK, ConnectionClosedError
+
+async def handler(websocket):
+    print("🔌 [OPEN] Client connected")
+    try:
+        async for message in websocket:
+            if isinstance(message, str):
+                print(f"📩 [RECEIVED] {message}")
+                response = f"Echo-{message}"
+                await websocket.send(response)
+                print(f"📤 [SENT] {response}")
+            else:
+                print("⚠️ [ERROR] Received non-text message")
+    except ConnectionClosedOK:
+        print("🔚 [CLOSED] Client closed connection normally (1000)")
+
+    except ConnectionClosedError as e:
+        print(f"⚠️ [ERROR] Abnormal disconnect (1006). Details: {e}")
+
+    except Exception as e:
+        print(f"🔥 [SERVER ERROR] {e}")
+
+    finally:
+        print(f"🏁 [FINALLY] Cleanup. Close code: {websocket.close_code}, Reason: {websocket.close_reason}")
+
+
+async def main():
+    server = await websockets.serve(handler, "localhost", 9001)
+    print("Server started ws://localhost:9001 abnd waiting for clients...")
+    # keeping the server forever running
+    await server.wait_closed()
+    print("Server fully stopped")
+
+asyncio.run(main())
