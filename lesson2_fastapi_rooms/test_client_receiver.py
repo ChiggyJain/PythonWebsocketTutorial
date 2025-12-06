78c9ea6ed265214b4cbc8b771b2217635e8e1e68
Chirag Jain <cjain9975@gmail.com>
2025-12-06 16:03:41 +0530

    Python Websocket Tutorial

--- lesson2_fastapi_rooms/test_client_receiver.py
+++ lesson2_fastapi_rooms/test_client_receiver.py
@@ -4,7 +4,7 @@ import websockets
 TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImNoaXJhZyJ9.Gff-Esi8_YMgcZVI-MYyxLroQZTiqHkdO4d5v7rVHwY"
 
 async def run():
-    ws = await websockets.connect(f"ws://localhost:9007/ws?token={TOKEN}")
+    ws = await websockets.connect(f"ws://localhost:8000/ws?token={TOKEN}")
     await ws.send('{"action":"join","room":"Room1"}')
     async for message in ws:
         print("📩", message)
