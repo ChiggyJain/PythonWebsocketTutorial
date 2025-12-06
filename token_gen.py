84e9570a12304c770bc26972ac66635d4fde3fb2
Chirag Jain <cjain9975@gmail.com>
2025-12-06 16:25:29 +0530

    Python Websocket Tutorial

--- token_gen.py
+++ token_gen.py
@@ -2,8 +2,8 @@ import jwt
 
 SECRET_KEY = "MY_SUPER_SECRET_KEY"
 
-token = jwt.encode({"username": "chirag"}, SECRET_KEY, algorithm="HS256")
-print("Your Token:", token)
+#token = jwt.encode({"username": "chirag"}, SECRET_KEY, algorithm="HS256")
+#print("Your Token:", token)
 
-#print(jwt.encode({"username": "chirag", "role": "student"}, SECRET_KEY, algorithm="HS256"))
-#print(jwt.encode({"username": "teacher1", "role": "teacher"}, SECRET_KEY, algorithm="HS256"))
+print(jwt.encode({"username": "chirag", "role": "student"}, SECRET_KEY, algorithm="HS256"))
+print(jwt.encode({"username": "teacher1", "role": "teacher"}, SECRET_KEY, algorithm="HS256"))
